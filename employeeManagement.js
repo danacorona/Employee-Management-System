00148ae07c368e1483866c619f28934c7a762519
Dana Corona <dana.d.corona@gmail.com>
2021-03-13 19:14:08 -0600

    Added the 'Add' queries to this homework and updated the schema with autoincrement

--- employeeManagement.js
+++ employeeManagement.js
@@ -1,5 +1,7 @@
-const mysql = require('mysql');
+//Dependencies
 const inquirer = require('inquirer');
+const mysql = require('mysql');
+
 
 const connection = mysql.createConnection ({
     host: 'localhost',
@@ -9,8 +11,132 @@ const connection = mysql.createConnection ({
     database: 'employee_manager_db',
 });
 
+// lets us know if there was an error with connection
 connection.connect((err) => {
     if (err) throw err;
-    console.log(`connected as id at ${connection.threadId}`);
-    connection.end();
-});
+    console.log(`connected as id ${connection.threadId}`);
+    runProgram();
+  });
+
+
+
+  const runProgram = () => {
+      inquirer
+        .prompt([
+            {
+                name: "programChoices",
+                type: "list",
+                message: "What would you like to do?",
+                choices: [
+                    "Add Department", "Add Role", "Add Employee",
+                    "View Departments", "View Roles", "View Employees",
+                    "Update Roles"
+                ]
+            }
+        ]).then((res) => {
+            switch (res.programChoices) {
+                case 'Add Department':
+                    addDepartment();
+                    break;
+
+                case 'Add Role':
+                    addRole();
+                    break;
+
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+
+                case 'View Departments':
+                    viewDepartment();
+                    break;
+
+                case 'View Roles':
+                    viewRoles();
+                    break;
+
+                case 'View Employees':
+                    viewEmployees();
+                    break;
+
+                case 'Update Roles':
+                    updateRoles();
+                    break;
+
+                default:
+                    console.log(`Invalid action: ${res.programChoices}`)
+                    break;
+            }
+        })
+  }
+
+
+    //Add Functions
+  const addDepartment = () => {
+    inquirer
+        .prompt ([
+            {
+                name: "departmentName",
+                message: "What is the Department Name?",
+            },
+        ]).then((res) => {   
+            connection.query(`INSERT INTO department (depName) VALUES ("${res.departmentName}")`)
+            console.log(`You have successfully added a department named ${res.departmentName}!`);
+            runProgram();
+        })
+    }
+
+  const addRole = () => {
+      inquirer
+        .prompt([
+            {
+                name: "title",
+                message: "What is the title of this role?"
+            },
+            {
+                name: "salary",
+                message: "What is the salary for this role?"
+            }
+        ]).then ((res) => {
+            connection.query(`INSERT INTO role (title, salary) VALUES ("${res.title}", ${res.salary})`)
+            console.log(`Added a role of ${res.title} and a salary of ${res.salary}!`);
+            runProgram();
+        })
+  }
+
+  const addEmployee = () => {
+      inquirer
+        .prompt([
+            {
+                name: "firstname",
+                message: "What is the Employee's First Name?"
+            },
+            {
+                name: "lastname",
+                message: "What is the Employee's Last Name?"
+            }
+        ]).then ((res) => {
+            connection.query(`INSERT INTO employee (first_name, last_name) VALUES ("${res.firstname}", "${res.lastname}")`)
+            console.log(`Added ${res.firstname} ${res.lastname}!`)
+            runProgram();
+        })
+  }
+
+
+
+
+
+  // View Functions (all) -- Bonus (Managers & combined salary)
+  const viewDepartment = () => {
+      
+  }
+
+
+  // Update Functions (roles) -- Bonus (Managers)
+
+
+  //Bonus Delete Functions
+
+
+  //End Connection
+
--- employeemanager.sql
+++ employeemanager.sql
@@ -5,19 +5,21 @@
     USE employee_manager_db;
 
     CREATE TABLE department (
-        name VARCHAR(30) NOT NULL,
-        id INTEGER NOT NULL,
+        depName VARCHAR(30) NOT NULL ,
+        id INTEGER NOT NULL AUTO_INCREMENT,
         PRIMARY KEY (id)
     );
 
     CREATE TABLE role (
-        id INTEGER NOT NULL,
+        id INTEGER NOT NULL AUTO_INCREMENT,
         title VARCHAR(30) NOT NULL,
-        salary DECIMAL (5,2) NOT NULL
+        salary DECIMAL (7,2) NOT NULL
+        PRIMARY KEY (id)
     );
 
     CREATE TABLE employee (
-        id INTEGER NOT NULL,
+        id INTEGER NOT NULL AUTO_INCREMENT,
         first_name VARCHAR(30) NOT NULL,
         last_name VARCHAR(30) NOT NULL
-    )
+        PRIMARY KEY (id)
+    );
